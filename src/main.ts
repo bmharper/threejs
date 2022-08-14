7fc4365d57d11116e80173018512022d5aa38f73
Ben Harper <rogojin@gmail.com>
2022-08-14 09:54:41 +0200

    Tried generating env from scene rendering

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import "./style.css";
 import * as THREE from "three";
 import { EXRLoader } from "three/examples/jsm/loaders/EXRLoader";
-import { text } from "stream/consumers";
 
 let width = 800;
 let height = 600;
 let dpr = window.devicePixelRatio;
 let appDomRoot = document.getElementById("app")!;
+let ownEnv = false;
 
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
@@ -36,7 +36,7 @@ let material = new THREE.MeshPhysicalMaterial({
 	//emissiveIntensity: 0.3,
 	color: 0xffffd0,
 	//color: 0xffffff,
-	roughness: 0.25,
+	roughness: 0.01,
 	metalness: 1,
 });
 let cube = new THREE.Mesh(geometry, material);
@@ -44,56 +44,124 @@ cube.rotation.x = 0.3;
 cube.rotation.y = 0.8;
 scene.add(cube);
 
-/*
-let light = new THREE.PointLight(0xff4444, 1.0);
-light.position.set(10, 0, 10);
-scene.add(light);
-
-light = new THREE.PointLight(0x44ff44, 1.0);
-light.position.set(-10, 0, 10);
-scene.add(light);
-
-light = new THREE.PointLight(0x4444ff, 1.0);
-light.position.set(0, 5, 0);
-scene.add(light);
-*/
-
 camera.position.z = 5;
 
-let pmremGenerator = new THREE.PMREMGenerator(renderer);
-pmremGenerator.compileEquirectangularShader();
+function createRGBLights() {
+	let light = new THREE.PointLight(0xff4444, 1.0);
+	light.position.set(10, 0, 10);
+	scene.add(light);
 
-let exrCubeRenderTarget: THREE.WebGLRenderTarget;
+	light = new THREE.PointLight(0x44ff44, 1.0);
+	light.position.set(-10, 0, 10);
+	scene.add(light);
+
+	light = new THREE.PointLight(0x4444ff, 1.0);
+	light.position.set(0, 5, 0);
+	scene.add(light);
+}
 
 async function loadEnvironment() {
+	let pmremGenerator = new THREE.PMREMGenerator(renderer); // dispose?
+	pmremGenerator.compileEquirectangularShader();
+
+	let exrCubeRenderTarget: THREE.WebGLRenderTarget; // must dispose
+
 	let loader = new EXRLoader();
 	//let texture = await loader.loadAsync("/textures/wooden_lounge_1k.exr");
 	let texture = await loader.loadAsync("/textures/pretville_cinema_2k.exr");
 	exrCubeRenderTarget = pmremGenerator.fromEquirectangular(texture);
 	texture.dispose();
+
+	// Not sure why this doesn't work:
 	//scene.environment = texture;
 
 	material.envMap = exrCubeRenderTarget.texture;
 	material.envMapIntensity = 1.0;
 	material.needsUpdate = true;
-	/*
-	let material = new THREE.MeshPhysicalMaterial({
-		color: 0xffffff,
-		roughness: 0.1,
-		metalness: 0.9,
-		envMapIntensity: 1.0,
-		envMap: texture,
-	});
-	scene.remove(cube);
-
-	cube = new THREE.Mesh(geometry, material);
-	cube.rotation.x = 0.3;
-	cube.rotation.y = 0.8;
-	scene.add(cube);
-	*/
 }
 
-loadEnvironment();
+function generateEnvironment() {
+	let scene = new THREE.Scene();
+
+	let nBulbRows = 4;
+	let nBulbs = 7;
+	let anglePerBulb = (Math.PI * 2) / nBulbs;
+	let bulbFill = 0.85;
+	let bulbRadius = 10;
+	let bulbRowHeight = 10;
+	let bulbAngleJitter = 0.2;
+	let bulbSizeJitter = 0.9;
+
+	let bulbMaterials: THREE.MeshPhysicalMaterial[] = [];
+	let materialColors = [0xffffff, 0xffffff, 0xffeeee, 0xffff88];
+	let materialStrengths = [1, 2, 4, 8, 16].map((x) => x * 0.7);
+	for (let i = 0; i < 31; i++) {
+		bulbMaterials.push(
+			new THREE.MeshPhysicalMaterial({
+				emissive: materialColors[i % materialColors.length],
+				emissiveIntensity: materialStrengths[i % materialStrengths.length],
+			})
+		);
+	}
+
+	let whiteMaterial = new THREE.MeshPhysicalMaterial({
+		emissive: 0xf8f8ff,
+		emissiveIntensity: 0.1,
+	});
+
+	let redMaterial = new THREE.MeshPhysicalMaterial({
+		emissive: 0xff0000,
+		emissiveIntensity: 8,
+	});
+
+	let im = 0;
+	let height = bulbRowHeight * (nBulbRows / 2);
+	for (let row = 0; row < nBulbRows; row++) {
+		let thetaInc = (Math.PI * 2) / nBulbs;
+		for (let theta = 0; theta < Math.PI * 2; theta += thetaInc) {
+			let th = theta + (Math.random() - 0.5) * bulbAngleJitter * thetaInc;
+			let r = anglePerBulb * bulbFill * bulbRadius * 0.5;
+			r *= 1.0 + (Math.random() - 0.5) * bulbSizeJitter;
+			let geometry = new THREE.SphereGeometry(r, 32, 32);
+			let sphere = new THREE.Mesh(geometry, bulbMaterials[im % bulbMaterials.length]);
+			sphere.position.set(bulbRadius * Math.cos(th), height, bulbRadius * Math.sin(th));
+			scene.add(sphere);
+			im++;
+		}
+		height -= bulbRowHeight;
+	}
+
+	let ringsHeight = 70;
+	let nRings = 5;
+	for (let i = 0; i < nRings; i++) {
+		let geometry = new THREE.TorusGeometry(50, 15, 32, 32);
+		let ring = new THREE.Mesh(geometry, bulbMaterials[im % bulbMaterials.length]);
+		im++;
+		//if (i === 3) ring.material = redMaterial;
+		ring.position.set(0, 0, -ringsHeight / 2 + (i * ringsHeight) / nRings);
+		ring.rotation.x = i * 0.05;
+		ring.rotation.y = i * 0.03;
+		scene.add(ring);
+	}
+
+	let pmremGenerator = new THREE.PMREMGenerator(renderer); // dispose?
+	pmremGenerator.compileCubemapShader();
+
+	let exrCubeRenderTarget: THREE.WebGLRenderTarget; // must dispose
+
+	exrCubeRenderTarget = pmremGenerator.fromScene(scene, 0.05, 0.1, 100);
+
+	material.envMap = exrCubeRenderTarget.texture;
+	material.envMapIntensity = 1.0;
+	material.needsUpdate = true;
+}
+
+if (ownEnv) {
+	//createRGBLights();
+	generateEnvironment();
+} else {
+	loadEnvironment();
+}
 
 function animate() {
 	requestAnimationFrame(animate);
